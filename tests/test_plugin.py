45a847e0a619e027b747d0af1f00d6e7646e9399
Wesley Miller <37603175+wmiller4@users.noreply.github.com>
2024-11-22 23:17:07 -0600

    Catch PyProjectException in root lookup
    If parent pyproject.toml is not a Poetry project, return None.

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,9 +1,25 @@
 from pathlib import Path
 
 from cleo.testers.application_tester import ApplicationTester
+from poetry.console.commands.command import Command
 
+from poetry_multiverse_plugin.hooks.hook import Hook
+from poetry_multiverse_plugin.plugin import PluginConfig
+from poetry_multiverse_plugin.workspace import Workspace
 from tests.conftest import ProjectFactory
 from tests.utils import MockApplication
+
+
+class BrokenHook(Hook):
+    def run(self, workspace: Workspace, command: Command):
+        raise Exception('BrokenHook is broken!')
+    
+    @staticmethod
+    def with_command_names(*names: str) -> 'BrokenHook':
+        class HookImpl(BrokenHook):
+            command_names = set(names)
+
+        return HookImpl()
 
 
 def test_no_workspace(project: ProjectFactory):
@@ -23,5 +39,18 @@ def test_workspace(project: ProjectFactory):
 def test_non_poetry_parent(project: ProjectFactory, tmp_path: Path):
     (tmp_path / 'pyproject.toml').touch()
     tester = ApplicationTester(MockApplication(project('p1')))
+    assert tester.execute('lock') == 0
+
+
+def test_hook_exception_handler(project: ProjectFactory):
+    def broken_hook() -> BrokenHook:
+        return BrokenHook.with_command_names('lock')
+
+    project(workspace_root=True)
+    p1 = project('p1')
+    app = MockApplication(p1)
+    PluginConfig(commands=[], pre_hooks=[broken_hook]).configure(app)
+    tester = ApplicationTester(app)
+
     assert tester.execute('lock') == 0
     assert 'Multiverse' in tester.io.fetch_error()

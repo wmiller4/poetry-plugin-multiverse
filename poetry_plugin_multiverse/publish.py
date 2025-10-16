15a700e51bb44aa9161716e4710cc9ff07affa80
Wesley Miller <37603175+wmiller4@users.noreply.github.com>
2025-10-15 23:45:53 -0500

    Fix project extras in wheel metadata
    Optional dependencies on workspace projects were missing in wheel metadata.

--- poetry_plugin_multiverse/publish.py
+++ poetry_plugin_multiverse/publish.py
@@ -1,5 +1,8 @@
 from typing import Iterable, Optional
+
+from packaging.utils import NormalizedName
 from poetry.core.packages.dependency import Dependency
+from poetry.core.packages.dependency_group import MAIN_GROUP
 from poetry.core.packages.path_dependency import PathDependency
 from poetry.poetry import Poetry
 
@@ -14,13 +17,24 @@ class PublishedDependencies:
     def __call__(self, dep: Dependency) -> Optional[Dependency]:
         if isinstance(dep, PathDependency):
             if project := self.projects.get(dep.full_path):
-                return Dependency(
+                external = Dependency(
                     dep.name,
                     f'^{project.package.version}',
                     optional=dep.is_optional(),
                     groups=dep.groups,
                     extras=dep.extras
                 )
+                external.marker = dep.marker
+                return external
+        return None
+    
+    def extra(self, extra: NormalizedName, dep: Dependency) -> Optional[Dependency]:
+        if external := self(dep):
+            external._optional = True
+            external._in_extras = [extra]
+            if dep.is_activated():
+                external.activate()
+            return external
         return None
     
     def patch_project(self, poetry: Poetry):
@@ -31,3 +45,15 @@ class PublishedDependencies:
                 if published := self(dep):
                     group.remove_dependency(dep.name)
                     group.add_dependency(published)
+        poetry.package.extras = {
+            name: [
+                self.extra(name, dep) or dep
+                for dep in dependencies
+            ]
+            for name, dependencies in poetry.package.extras.items()
+        }
+        main_group = poetry.package.dependency_group(MAIN_GROUP)
+        for dependencies in poetry.package.extras.values():
+            for dep in dependencies:
+                main_group.remove_dependency(dep.name)
+                main_group.add_dependency(dep)
--- tests/test_publish.py
+++ tests/test_publish.py
@@ -1,9 +1,36 @@
+from packaging.utils import canonicalize_name
+from poetry.console.commands.add import AddCommand
 from poetry.core.constraints.version.version import Version
 from poetry.core.packages.package import Package
+from poetry.poetry import Poetry
 
 from poetry_plugin_multiverse.publish import PublishedDependencies
+from poetry_plugin_multiverse.workspace import Workspace
 from tests import utils
 from tests.conftest import ProjectFactory
+
+
+def add_optional_extra() -> bool:
+    return any(
+        option.name == 'optional' and not option.is_flag()
+        for option in AddCommand.options
+    )
+
+
+def add_optional(workspace: Workspace, project: Poetry, dependency: str, extra: str) -> Poetry:
+    if add_optional_extra():
+        return utils.add(project, dependency, f'--optional={extra}')
+
+    utils.add(project, dependency, '--optional')
+    with project.pyproject_path.open('a') as out:
+        out.write(f'''
+[tool.poetry.extras]
+{extra} = ["{dependency.split('/')[-1]}"]
+''')
+    return [
+        child for child in workspace.projects
+        if child.package.name == project.package.name
+    ][0]
 
 
 def test_patch_versions(project: ProjectFactory):
@@ -17,6 +44,20 @@ def test_patch_versions(project: ProjectFactory):
     assert p2.package.all_requires[0].name == p1.package.name
     assert p2.package.all_requires[0].constraint.allows(p1.package.version) is True
     assert p2.package.all_requires[0].constraint.allows(Version.from_parts(1, 2, 33)) is False
+
+
+def test_patch_versions_optional(project: ProjectFactory):
+    p1 = project('p1')
+    p1.package.version = Version.from_parts(1, 2, 34)
+
+    p2 = add_optional(project.workspace(p1), project('p2'), '../p1', 'test')
+    PublishedDependencies([p1, p2]).patch_project(p2)
+
+    dependency = p2.package.extras[canonicalize_name('test')][0]
+    assert dependency.name == p1.package.name
+    assert dependency.constraint.allows(p1.package.version) is True
+    assert dependency.constraint.allows(Version.from_parts(1, 2, 33)) is False
+    assert dependency.in_extras == ['test']
 
 
 def test_ignore_external_deps(project: ProjectFactory):
